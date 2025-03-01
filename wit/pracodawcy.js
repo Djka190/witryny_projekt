ec3a9b5f572d1f68d639611318896bd1f0011c47
ruster-mw <mateuszwypior2000@gmail.com>
2025-03-01 19:10:03 +0100

    Add files via upload

---
+++ wit/pracodawcy.js
@@ -0,0 +1,19 @@
+let ofertyPracy = document.querySelectorAll(".pracodawca")
+let ofertyPracySection = document.querySelector(".pracodawca-sekcja")
+let pracodawcyForm = document.querySelector("#centrum")
+let pracodawcyInput = document.querySelector("#input")
+let pracodawcyInputValue
+
+pracodawcyForm.addEventListener('keyup',function(e){
+    e.preventDefault()
+    pracodawcyInputValue = pracodawcyInput.value
+    ofertyPracy.forEach(item => {
+        console.log(item.textContent.trim().toLowerCase().includes(pracodawcyInputValue.toLowerCase()))
+        if(item.textContent.trim().toLowerCase().includes(pracodawcyInputValue.toLowerCase())){
+            console.log("hello?")
+            item.style.display = "flex"
+        }else{
+            item.style.display = "none"
+        }
+    })
+})
